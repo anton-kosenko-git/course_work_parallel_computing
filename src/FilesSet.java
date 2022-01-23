e12f9620cd10525cb58075ee8498063f9a239e8c
Anton Kosenko <kosenko.mvp@gmail.com>
2022-01-23 14:50:55 +0200

    Add class for directory traversal

---
+++ src/FilesSet.java
@@ -0,0 +1,46 @@
+import java.io.File;
+import java.util.ArrayList;
+
+public class FilesSet {
+
+    public void setFilepath(String filepath) {
+        this.filepath = filepath;
+    }
+
+    private String filepath = "data\\aclImdb\\";
+    private int variant = 5;
+
+    private static ArrayList<File> files = new ArrayList<>();
+    private static ArrayList<File> folders = new ArrayList<>();
+
+    private static void readFiles(File[] folders, int n, int variant) {
+        for (int FileBeginWith = n / 50 * variant; FileBeginWith < n / 50 * (variant + 1); FileBeginWith++) {
+            for (File file : folders) {
+                if (file.getName().startsWith(String.valueOf(FileBeginWith))) {
+                    files.add(file.getAbsoluteFile());
+                }
+            }
+        }
+    }
+
+    //read files
+    public ArrayList<File> getFiles() {
+
+        //Folders with rating
+        folders.add(new File(filepath + "train\\neg"));
+        folders.add(new File(filepath + "test\\pos"));
+        folders.add(new File(filepath + "train\\neg"));
+        folders.add(new File(filepath + "train\\pos"));
+        //Folder contains files w/o rating
+        folders.add(new File(filepath + "train\\unsup"));
+
+        //read files from all folders
+        for (int processingFolder = 0; processingFolder < folders.size() - 1; processingFolder++) {
+            readFiles(folders.get(processingFolder).listFiles(), 12500, variant);
+        }
+        readFiles(folders.get(4).listFiles(), 50000, variant);
+
+        return files;
+    }
+
+}
